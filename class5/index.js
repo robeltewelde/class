11aa58890d74a1e10b7227d86eff3796de1fbccc
robeltewelde <149680946+robeltewelde@users.noreply.github.com>
2023-12-05 15:44:56 +0300

    up dat

---
+++ class5/index.js
@@ -0,0 +1,35 @@
+const obj = [
+    {
+        img: "",
+        p: "this is a paragraph",
+
+
+
+
+
+
+
+
+
+        
+    }, 
+    {
+        img: "",
+        p:"this is another one"
+    },
+    {
+        img: "",
+        p:"this is another one"
+    }
+]
+const ul = [
+    {
+        name: "Home",
+        link: ""
+    }
+]
+
+
+
+
+
